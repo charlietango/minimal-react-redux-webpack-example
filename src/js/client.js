b53abb7a5b06fa411071e46689df04f9f50fcfec
Catalin Tudorache <charlietango592>
2017-01-11 21:48:54 +0200

    :star: Added a dummy component & fixed package.json

---
+++ src/js/client.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Ahoi from "./components/Ahoi";
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Ahoi />, app);
