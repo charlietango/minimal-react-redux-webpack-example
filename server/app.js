7874f23d95921fc2742fa727584f47b137bff4f2
Catalin Tudorache <charlietango592>
2017-01-27 18:53:04 +0200

    Added express

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+// Setup logger
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+
+// Serve static assets
+app.use('/build', express.static(path.resolve(__dirname, '..', 'src')));
+
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'src', 'index.html'));
+});
+
+module.exports = app;
